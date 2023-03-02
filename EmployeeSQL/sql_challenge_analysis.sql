c66480517ef163a42cf943c321bae58f9b146832
sewiteskinder <sewit.eskinder@gmail.com>
2023-03-01 18:31:28 -0600

    comments added

--- EmployeeSQL/sql_challenge_analysis.sql
+++ EmployeeSQL/sql_challenge_analysis.sql
@@ -1,3 +1,4 @@
+-- list the employee number, last name, first name, sex, and salary of each employee using a JOIN
 SELECT  e.emp_no,
         e.last_name,
         e.first_name,
@@ -8,12 +9,14 @@ LEFT JOIN salaries as s
 ON (e.emp_no = s.emp_no)
 ORDER BY e.emp_no ;
 
+-- list the first name, last name, and hire date for employees that were hired in 1986
 SELECT  e.first_name,
 		e.last_name,
 		e.hire_date
 FROM employees as e
 WHERE e.hire_date BETWEEN '1/1/1986' AND '12/31/1986' ;
 
+-- list the manager for each department along with their department number, department name, employee number, first name, and last name
 SELECT  dm.dept_no,
 		d.dept_name,
 		e.emp_no,
@@ -22,6 +25,7 @@ SELECT  dm.dept_no,
 FROM dept_manager as dm , employees as e , departments as d
 WHERE dm.emp_no = e.emp_no AND dm.dept_no = d.dept_no ;
 
+-- list the department number for each employee along with their employee number, last name, first name, and department name
 SELECT  de.dept_no,
 		e.emp_no,
 		e.last_name,
@@ -30,12 +34,15 @@ SELECT  de.dept_no,
 FROM dept_emp as de , employees as e , departments as d
 WHERE de.dept_no = d.dept_no AND de.emp_no = e.emp_no ;
 
+-- list the first name, last name, and sex of each employee with the first name 'Hercules' and last name beginning with the letter 'B' 
+-- note: you will have to use a Wildcard
 SELECT  e.first_name,
 		e.last_name,
 		e.sex
 FROM employees as e
 WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%' ;
 
+-- list each employee from the 'Sales' department along with their employee number, last name, and first name
 SELECT  de.emp_no,
 		e.last_name,
 		e.first_name,
@@ -43,6 +50,8 @@ SELECT  de.emp_no,
 FROM dept_emp as de , employees as e , departments as d
 WHERE de.emp_no = e.emp_no AND de.dept_no = d.dept_no AND d.dept_name = 'Sales' ;
 
+-- list each employee from the 'Sales' and 'Development' departments (aggregate list of both) including their employee number, last name, and first name
+-- note: use JOINs to be able to access all the necessary information 
 SELECT  de.emp_no,
 		e.last_name,
 		e.first_name,
@@ -54,6 +63,7 @@ LEFT JOIN departments as d
 ON (de.dept_no = d.dept_no)
 WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development' ;
 
+-- list all of the employees last names with frequency counts running in descending order (how many employees share each last name)
 SELECT e.last_name, COUNT(e.last_name)
 FROM employees as e 
 GROUP BY e.last_name
--- EmployeeSQL/sql_challenge_engineering.sql
+++ EmployeeSQL/sql_challenge_engineering.sql
@@ -1,15 +1,18 @@
+-- create 'titles' table, specifying the primary key
 CREATE TABLE titles(
 	title_id VARCHAR(10) NOT NULL,
     title VARCHAR(25) NOT NULL,
     PRIMARY KEY (title_id)
 );
 
+-- create 'departments' table, specifying the primary key
 CREATE TABLE departments(
 	dept_no VARCHAR(10) NOT NULL,
 	dept_name VARCHAR(25) NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
+-- create 'employees' table, specifying the primary and foreign keys
 CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	emp_title_id VARCHAR(10) NOT NULL,
@@ -22,6 +25,7 @@ CREATE TABLE employees(
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
 
+-- create 'dept_manager' table, specifying the composite and foreign keys
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(10) NOT NULL, 
 	emp_no INT NOT NULL, 
@@ -30,6 +34,7 @@ CREATE TABLE dept_manager(
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+-- create 'dept_emp' table, specifying the composite and foreign keys
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(10) NOT NULL,  
@@ -38,6 +43,7 @@ CREATE TABLE dept_emp(
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+-- create 'salaries' table, specifying the primary key
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
